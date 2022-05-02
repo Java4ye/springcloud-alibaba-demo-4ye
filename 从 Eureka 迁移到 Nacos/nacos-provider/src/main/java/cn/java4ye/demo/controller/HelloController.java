42ffe6b4865d953c2511e008d0e9d9a9f74e85f6
Java4ye <ryzeyang@foxmail.com>
2022-05-02 18:20:42 +0800

    双注册双订阅模式迁移Eureka到Nacos

---
+++ 从 Eureka 迁移到 Nacos/nacos-provider/src/main/java/cn/java4ye/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package cn.java4ye.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Java4ye
+ * @微信公众号： Java4ye
+ * @GitHub https://github.com/Java4ye
+ * @CSDN https://blog.csdn.net/weixin_40251892
+ * @掘金 https://juejin.cn/user/2304992131153981
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello/{name}")
+    public String sayHi(@PathVariable String name) {
+        return "[4ye]Hello nacos discovery " + name;
+    }
+}
